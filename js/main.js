7dcf89ace4b2f03ad2864cd4ad55bf5a2165fe05
Mick Thompson <dthompson@gmail.com>
2013-04-12 15:52:40 -0700

    billboard logo. set image to cover

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ socket.on('display', function (data) {
 
   if(data.action === "image"){
     $("div.showImage").css({"background-image": "url('"+data.url+"')",
-                            "background-size": "contain",
+                            "background-size": "cover",
                             "background-repeat": "no-repeat"});
     $("div.showImage").addClass("show");
   }
